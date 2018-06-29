7091aca54fe0ca480a38fb1d887ba49326ad270b
Unknown <meme.f14@gmail.com>
2018-06-29 12:08:21 -0500

    Auto stash before merge of "master" and "origin/master"

--- TequilaServer/mappers/MapperGenero.rb
+++ TequilaServer/mappers/MapperGenero.rb
@@ -156,4 +156,6 @@ class MapperGenero
 	   	canciones 
 	end
 
+	
+
 end
--- TequilaServer/mappers/MapperPlaylist.rb
+++ TequilaServer/mappers/MapperPlaylist.rb
@@ -45,7 +45,18 @@ class MapperPlaylist
 		begin
 			conexion = Conexion.new
 			con = conexion.conectar
+<<<<<<< Updated upstream
 			consulta = con.prepare("SELECT Cancion.idCancion, Cancion.titulo as nombreCancion, Cancion.ruta,  Album.titulo as nombreAlbum, Album.idAlbum, Album.anioLanzamiento, Album.companiaDiscografica, Genero.idGenero, Genero.nombreGenero, Usuario.correo  from Cancion join Album join Usuario join Genero join Contenido join Playlist where Cancion.idAlbum = Album.idAlbum and Genero.idGenero = Cancion.idGenero and Album.correo = Usuario.correo and Cancion.idCancion = Contenido.idCancion and Contenido.idPlaylist = Playlist.idPlaylist and Contenido.idPlaylist = ?;")
+=======
+			consulta = con.prepare("SELECT Cancion.idCancion, Cancion.titulo as nombreCancion, 
+				Cancion.ruta,  Album.titulo as nombreAlbum, Album.idAlbum, Album.anioLanzamiento,
+				Album.companiaDiscografica, Album.imagenAlbum, Genero.idGenero, Genero.nombreGenero, 
+				Usuario.correo,
+				from Cancion join Album join Usuario join Genero join Contenido join Playlist
+				where Cancion.idAlbum = Album.idAlbum and Genero.idGenero = Cancion.idGenero 
+				and Album.correo = Usuario.correo and Cancion.idCancion = Contenido.idCancion and 
+				Contenido.idPlaylist = Playlist.idPlaylist and Contenido.idPlaylist = ?")
+>>>>>>> Stashed changes
 			resultado = consulta.execute(idPlaylist)
 			
 			resultado.each do |registro|
@@ -55,30 +66,22 @@ class MapperPlaylist
 				registro["nombreAlbum"] = "NULL" if registro["nombreAlbum"].nil?
 				registro["idAlbum"] = "NULL" if registro["idAlbum"].nil?
 				registro["imagenAlbum"] = "NULL" if registro["imagenAlbum"].nil?
-				registro["nombreUsuario"] = "NULL" if registro["nombreUsuario"].nil?
 				registro["idGenero"] = "NULL" if registro["idGenero"].nil?
 				registro["nombreGenero"] = "NULL" if registro["nombreGenero"].nil?
-				registro["correo"] = "NULL" if registro["correo"].nil?
 				registro["anioLanzamiento"] = "NULL" if registro["anioLanzamiento"].nil?
 				registro["companiaDiscografica"] = "NULL" if registro["companiaDiscografica"].nil?
-				registro["puntuacion"] = "NULL" if registro["puntuacion"].nil?
-				registro["descargada"] = "NULL" if registro["descargada"].nil?
 
 				cancion = CancionSL.new 
 				cancion.idCancion = registro["idCancion"]
 				cancion.titulo = registro["nombreCancion"]
 				cancion.ruta = registro["ruta"]
 				cancion.album = registro["nombreAlbum"]
-				cancion.artista = registro["nombreUsuario"]
-				cancion.correoArtista = registro["correo"]
 				cancion.idAlbum = registro["idAlbum"]
 				cancion.idGenero = registro["idGenero"]
 				cancion.genero = registro["nombreGenero"]
 				cancion.imagenAlbum = registro["imagenAlbum"]
 				cancion.anioLanzamiento = registro["anioLanzamiento"]
 				cancion.companiaDiscografica = registro["companiaDiscografica"]
-				cancion.puntuacion = registro["puntuacion"]
-				cancion.descargada = registro["descargada"]
 				canciones << cancion
 	   		end
 
