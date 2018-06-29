35789373a6d4d05b0178ebdb58eee547984e290e
Alan Yoset Garcia Cruz <alancrux_@hotmail.com>
2018-06-29 12:49:47 -0500

    CAMBIO EN LA CONSULTA PLAYLIST

--- TequilaServer/mappers/MapperPlaylist.rb
+++ TequilaServer/mappers/MapperPlaylist.rb
@@ -45,18 +45,8 @@ class MapperPlaylist
 		begin
 			conexion = Conexion.new
 			con = conexion.conectar
-<<<<<<< Updated upstream
 			consulta = con.prepare("SELECT Cancion.idCancion, Cancion.titulo as nombreCancion, Cancion.ruta,  Album.titulo as nombreAlbum, Album.idAlbum, Album.anioLanzamiento, Album.companiaDiscografica, Genero.idGenero, Genero.nombreGenero, Usuario.correo  from Cancion join Album join Usuario join Genero join Contenido join Playlist where Cancion.idAlbum = Album.idAlbum and Genero.idGenero = Cancion.idGenero and Album.correo = Usuario.correo and Cancion.idCancion = Contenido.idCancion and Contenido.idPlaylist = Playlist.idPlaylist and Contenido.idPlaylist = ?;")
-=======
-			consulta = con.prepare("SELECT Cancion.idCancion, Cancion.titulo as nombreCancion, 
-				Cancion.ruta,  Album.titulo as nombreAlbum, Album.idAlbum, Album.anioLanzamiento,
-				Album.companiaDiscografica, Album.imagenAlbum, Genero.idGenero, Genero.nombreGenero, 
-				Usuario.correo,
-				from Cancion join Album join Usuario join Genero join Contenido join Playlist
-				where Cancion.idAlbum = Album.idAlbum and Genero.idGenero = Cancion.idGenero 
-				and Album.correo = Usuario.correo and Cancion.idCancion = Contenido.idCancion and 
-				Contenido.idPlaylist = Playlist.idPlaylist and Contenido.idPlaylist = ?")
->>>>>>> Stashed changes
+
 			resultado = consulta.execute(idPlaylist)
 			
 			resultado.each do |registro|
